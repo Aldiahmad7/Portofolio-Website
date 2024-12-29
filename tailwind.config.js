113e1b88b78cb5e03f3fcedae1ce07886a11a43f
aldi ahmad dani <aldiahmaddani7@outlook.com>
2024-12-30 00:57:54 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        white: '#ffffff'
+      }
+    },
+  },
+  plugins: [],
+}
+
