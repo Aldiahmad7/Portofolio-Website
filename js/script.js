ab4bc417b2c6e1516e86605f7b3fba3ea4f309a0
aldi ahmad dani <aldiahmaddani7@outlook.com>
2025-01-01 17:19:28 +0700

    baru

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+
+    // sticky header
+
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+
+    // animation footer on scroll
+
+}
